4306619b84c4f91f780d4e2f91c8795a59e0afb7
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2018-10-30 18:43:57 +0700

    bump version (1.0.1)

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="rtry",
     description="",
-    version="1.0.0",
+    version="1.0.1",
     url="https://github.com/nikitanovosibirsk/rtry",
     author="Nikita Tsvetkov",
     author_email="nikitanovosibirsk@yandex.com",
